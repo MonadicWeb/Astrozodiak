d62034cd05b037f7eb6a5ee58a2fc4c80c0cbf44
volodg <volodg@mail.ru>
2014-11-07 00:59:47 +0200

    test

--- migrationTools/Examples.hs
+++ migrationTools/Examples.hs
@@ -26,30 +26,37 @@ palindrome' lst = lst ++ (reverse lst)
 --         Just (y:ys) | x == y -> Just (ys) -- roll the tail back to the previous item
 --         _ -> Nothing
 
+trace'' :: String -> a -> a
+--trace'' str a = trace str a
+trace'' str a = a
+
 isPalindrome :: (Show a, Eq a) => [a] -> Bool
 isPalindrome lst = isPalindrome' lst [] [] where
     --isPalindrome' heads equals left
     isPalindrome' :: (Show a, Eq a) => [a] -> [a] -> [a] -> Bool
-    isPalindrome' []     [] [] = True
-    isPalindrome' []     _  [] = True
-    isPalindrome' []     _  _  = False
-    isPalindrome' [_]    [] [] = False
+    isPalindrome' []     [] [] = trace'' "True - a:[] b:[] c:[]" True
+    isPalindrome' []     e  [] = trace'' ("True - a:[] b:" ++ show e ++ " c:[]") True
+    isPalindrome' []     e  l  = trace'' ("False - a:[] b:" ++ show e ++ " c:" ++ show l) False
+    isPalindrome' [el]   [] [] = trace'' ("False - a:" ++ show el ++ " b:[] c:[]") False
     isPalindrome' (x:y:xs) e [] 
-        | x == y    = isPalindrome' xs     (x:e) []
-        | otherwise = isPalindrome' (y:xs) e     [x]
+        | x == y && null e = trace' 1 $ isPalindrome' xs     (x:e) []
+        | x == y           = trace' 2 $ isPalindrome' xs     [x]   (e ++ reverse e)
+        | otherwise        = trace' 3 $ isPalindrome' (y:xs) e     [x] where
+        trace' indx a = trace'' (show (indx) ++ " a:" ++ show (x:y:xs) ++ " b:" ++ show e ++ " c:[]") a
     isPalindrome' (x:xs) e (l:left)
-        | x == l    = trace' 1 $ isPalindrome' xs (x:e) left
-        | otherwise = trace' 2 $ isPalindrome' xs e     (x : l : left ++ e ++ reverse e) where
-            trace' indx a = trace (show (indx) ++ " a:" ++ show (x:xs) ++ " b:" ++ show e ++ " c:" ++ show (l:left)) a
+        | x == l    = trace' 11 $ isPalindrome' xs     (x:e) left
+        | null e    = trace' 12 $ isPalindrome' xs     []    (x : l : left)
+        | otherwise = trace' 13 $ isPalindrome' (x:xs) []    (e ++ reverse e ++ (l : left)) where
+            trace' indx a = trace'' (show (indx) ++ " a:" ++ show (x:xs) ++ " b:" ++ show e ++ " c:" ++ show (l:left)) a
 
 main = do
     --print "start"
-    -- print $ (reverse [] :: [Int])
-    -- print $ (isPalindrome ([] :: [Int])) == True
-    -- print $ isPalindrome [2] == False
-    -- print $ isPalindrome [2,2,3,2,2] == False
-    -- print $ isPalindrome [2,2,3,3,2,2] == True
-    -- print $ isPalindrome [4,1,2,2,1,5] == False
-    -- print $ isPalindrome [4,1,2,2,1,4] == True
-    -- print $ isPalindrome [4,1,2,3,2,1,4] == False
-    print $ (isPalindrome $ palindrome' [4,1,2,3,4,3,2,1]) == False
+    print $ (reverse [] :: [Int])
+    print $ (isPalindrome ([] :: [Int])) == True
+    print $ isPalindrome [2] == False
+    print $ isPalindrome [2,2,3,2,2] == False
+    print $ isPalindrome [2,2,3,3,2,2] == True
+    print $ isPalindrome [4,1,2,2,1,5] == False
+    print $ isPalindrome [4,1,2,2,1,4] == True
+    print $ isPalindrome [4,1,2,3,2,1,4] == False
+    print $ (isPalindrome $ palindrome' [4,1,2,2,1]) == True
